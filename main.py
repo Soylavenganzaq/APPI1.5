45a4ededde8ecc20abd17d6b48a79597506605bc
Soylavenganzaq <159969373+Soylavenganzaq@users.noreply.github.com>
2025-09-27 00:59:19 +0000

    guardar cambios

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Configurar JWT
 app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 app.config['JWT_TOKEN_LOCATION'] = JWT_TOKEN_LOCATION
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES #autenticación con el token
 app.config['JWT_HEADER_NAME'] = JWT_HEADER_NAME
 app.config['JWT_HEADER_TYPE'] = JWT_HEADER_TYPE
 
