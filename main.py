ec6a1e5fe00a493b00aef7c8d79d95b108c523b3
Soylavenganzaq <159969373+Soylavenganzaq@users.noreply.github.com>
2025-09-27 00:26:56 +0000

    dicen que cacha me la agita

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from config.jwt import *
 from controllers.component_controller import computador_bp
-
+from controllers.user_controllers import user_bp
 app = Flask(__name__)
+
+
+
+# Configurar JWT
+app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
+app.config['JWT_TOKEN_LOCATION'] = JWT_TOKEN_LOCATION
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
+app.config['JWT_HEADER_NAME'] = JWT_HEADER_NAME
+app.config['JWT_HEADER_TYPE'] = JWT_HEADER_TYPE
 
 # Registrar el blueprint de bandas
 app.register_blueprint(computador_bp)
-
+app.register_blueprint(user_bp)
 if __name__ == "__main__":
     app.run(debug=True)
